3ef895b42e51e9e74b3a26c4248cab96791c223a
RadwaAhmed4869 <48102202+RadwaAhmed4869@users.noreply.github.com>
2024-08-06 17:42:10 +0300

    52

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import TabButton from "./Components/TabButton";
 import { CORE_CONCEPTS } from "./data";
 
 function App() {
-  function handleSelect() {
-    console.log(`Hello World!`);
+  function handleSelect(selectedButton) {
+    console.log(selectedButton);
   }
 
   return (
@@ -29,10 +29,12 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={handleSelect}>Components</TabButton>
-            <TabButton onSelect={handleSelect}>JSX</TabButton>
-            <TabButton onSelect={handleSelect}>Props</TabButton>
-            <TabButton onSelect={handleSelect}>State</TabButton>
+            <TabButton onSelect={() => handleSelect("Components")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("JSX")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("Props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("State")}>State</TabButton>
           </menu>
         </section>
       </main>
